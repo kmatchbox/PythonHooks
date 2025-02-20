537de8365c4e22dd166dc877c167167c11129d29
Kyle Obley <kyle@kyleobley.com>
2025-02-20 12:05:55 +0100

    Initial git push: v0.1

---
+++ fix_openclip/README.md
@@ -0,0 +1,10 @@
+# Fix openClip Name
+
+Often Flame will use <shot name> or <batch iteration> as the entry for <name> in the openClip file. When you re-import said openClip the name that appers within Flame is whatever <name> is as opposed to the acutal file name of the .clip. This is not ideal for those who use only shot numbers for <shot name> as opposed to whatever_#### or if you want the name to not be stuck to the first iteration you exported.
+
+### How To Use:
+1. Select the openClip(s) in the Media Panel
+2. Right-click > Fix openClip > Fix openClip Name
+
+### What It Does
+Replaces the existing <name> field with the basename of the .clip
---
+++ fix_openclip/fix_openclip.py
@@ -0,0 +1,64 @@
+"""
+Script Name: Fix openClip
+Script Version: 0.1
+Flame Version: 2023
+
+Creation date: 05.01.24
+Modified date: 05.01.24
+
+Description:
+
+    Changes the <name> field in an openClip to match the filename.
+
+Change Log:
+
+    v0.1: Initial Release
+
+"""
+
+def fix_openclip(selection):
+    import os
+    import xml.etree.ElementTree as ET
+
+    for item in selection:
+        file = item.path
+        basename = os.path.splitext(os.path.basename(file))[0]
+
+        # Load XML
+        tree = ET.parse(file)
+        root = tree.getroot()
+
+        for child in root:
+
+            # Find the root instance of name which is what we're after
+            if child.tag == "name":
+                print ("File:     ", file)
+                print ("Original: ", child.text)
+                print ("Modified: ", basename)
+
+                child.text = basename
+
+                # Write out the modified XML
+                tree.write(file)
+
+def is_clip(selection):
+    import os
+    for item in selection:
+        if os.path.splitext(item.path)[1] == '.clip':
+            return True
+        else:
+            return False
+
+def get_mediahub_files_custom_ui_actions():
+    return [
+        {
+            "name": "Fix openClip",
+            "actions": [
+                {
+                    "name": "Fix openClip Name",
+                    "execute": fix_openclip,
+                    "isVisible": is_clip,
+                }
+            ]
+        }
+    ]
